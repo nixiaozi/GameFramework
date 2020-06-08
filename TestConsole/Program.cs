89219c231c739152836681eb167bca695ec837d6
lee cheng <nixiaozi01@hotmail.com>
2020-06-09 02:28:05 +0800

    测试gameframework底层接口映射模块的实现 发现只是使用了字符串拼接和反射，需要接口和类型命名规则缺少灵活性

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using GameFramework;
+using GameFramework.DataTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 测试如何通过获得接口类型（没有使用autofac的情况下）
+            // 第一次 GameFrameworkEntry.GetModule<T>方法中
+            // Utility.Text.Format("{0}.{1}", interfaceType.Namespace, interfaceType.Name.Substring(1)); 获得接口名的对应实体
+            // 最后使用反射找到对应类
+            GameFrameworkEntry.GetModule<IDataTableManager>();  
+        }
+    }
+}
